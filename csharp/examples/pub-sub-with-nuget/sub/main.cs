ecd4f452c4abc1f7eee167b39d7a8a92c9e291b8
ys-zhao <yunsheng.zhao@bydeluxe.com>
2018-06-14 16:43:14 -0700

    updated csharp project

---
+++ csharp/examples/pub-sub-with-nuget/sub/main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Deluxe.One.Nats;
+using System.Threading;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main()
+        {
+            string serverURL = "nats://localhost:4222", clusterID = "test-cluster", clientID = "sub_client", subject = "foo_subject", queue = "foo_queue", durable = "foo_durable";
+            nats.Connect(serverURL, clusterID, clientID);
+            nats.QueueSubscribe(subject, queue, durable, (sender, args)=>{
+                Console.WriteLine("Received seq #{0}: {1}", args.Message.Sequence, System.Text.Encoding.UTF8.GetString(args.Message.Data));
+            });
+             // wait for exit
+            var ev = new AutoResetEvent(false);
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                nats.Close();
+                // return
+                e.Cancel = true;
+                ev.Set();
+            };
+            Console.WriteLine("program: ctrl+c to exit...");
+            ev.WaitOne();
+        }
+    }
+}
